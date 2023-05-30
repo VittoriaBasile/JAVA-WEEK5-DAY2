edd2cbff84beef2a89b3065c1cb8b64a01f4f7fc
VittoriaBasile <vichy.basile@gmail.com>
2023-05-30 12:13:51 +0200

    configurazioni iniziali

---
+++ src/main/java/epicode/JAVAWEEK5DAY2/JavaWeek5Day2Application.java
@@ -0,0 +1,13 @@
+package epicode.JAVAWEEK5DAY2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeek5Day2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWeek5Day2Application.class, args);
+	}
+
+}
---
+++ src/test/java/epicode/JAVAWEEK5DAY2/JavaWeek5Day2ApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.JAVAWEEK5DAY2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeek5Day2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
